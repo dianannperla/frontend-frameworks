52f5da70a0c004425ce379848fd28ffabe722d94
dianannperla <waafirmelyza@hotmail.com>
2025-09-24 15:13:15 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Chào mừng đến với Frontend Frameworks!</h1>
+      <Button label="Nhấn vào đây" />
+    </div>
+  );
+}
+
+export default App;
